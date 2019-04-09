e7670402f7826972f6388375a58c7d6c76bc0359
Tauri Kirsipuu <46881542+taurikirsipuu@users.noreply.github.com>
2019-04-10 00:28:32 +0300

    files with independent work

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 /*jshint esversion:6*/
+let name = prompt("Palun sisesta enda nimi");
+
 class Typer{
-  constructor(){
-    this.wordsInGame = 5;
+  constructor(name){
+    this.name = name;
+    this.wordsInGame = 15;
     this.startingWordLength = 5;
     this.wordsTyped = 0;
-    this.wordDiv = $('#wordDiv');
     this.words = [];
     this.typeWords = [];
     this.word = "sõna";
+    this.startTime = 0;
+    this.endTime = 0;
+    this.results = JSON.parse(localStorage.getItem("score")) || [];
     this.init();
   }
 
   init(){
     $.get("lemmad2013.txt", (data)=>this.getWords(data));
+    $('#show-results').on('click', ()=>this.showResults());
   }
 
   getWords(data){
@@ -29,15 +35,16 @@ class Typer{
       }
       this.words[wordLength].push(data[i]);
     }
-    console.log(this.words);
+    //console.log(this.words);
     this.startTyper();
   }
 
   startTyper(){
-    console.log("asdasd");
     this.generateWords();
-
+    this.showInfo();
+    this.startTime = performance.now();
     $(document).on('keypress', (e)=>this.shortenWord(e.key));
+
   }
 
   generateWords(){
@@ -47,51 +54,85 @@ class Typer{
       const randomWord = Math.round(Math.random()*this.words[wordLength].length);
       this.typeWords[i] = this.words[wordLength][randomWord];
     }
-
-    console.log(this.typeWords);
+    //console.log(this.typeWords);
     this.selectWord();
   }
 
+  showInfo(){
+    $('#info').html(this.wordsTyped+"/"+this.wordsInGame);
+  }
+
   selectWord(){
+
       this.word = this.typeWords[this.wordsTyped];
       this.drawWord(this.word);
   }
 
   drawWord(){
-    console.log(this.wordsTyped, this.wordsInGame);
-    if(this.wordsTyped != this.wordsInGame){
-      this.wordDiv.html(this.word);
-    }else{
-      $(document).off('keypress');
-      this.startNewGame();
-    }
+    //console.log(this.wordsTyped, this.wordsInGame);
+    $('#wordDiv').html(this.word);
   }
 
   shortenWord(keypressed){
-    console.log(keypressed);
-    if(this.word.length > 1 && this.word.charAt(0)==keypressed){
+    //console.log(keypressed);
+    if(this.word.length > 1 && this.word.charAt(0) == keypressed){
       this.word = this.word.slice(1);
       this.drawWord();
-    } else if(this.word.length == 1 && this.word.charAt(0)==keypressed){
+    } else if(this.word.length == 1 && this.word.charAt(0) == keypressed && this.wordsTyped != this.wordsInGame-1){
       this.wordsTyped++;
-      this.generateWords();
+      this.showInfo();
+      this.selectWord();
+    } else if(this.word.charAt(0) != keypressed){
+      $("#container").css({"background-color":"red"});
+      setTimeout(function(){
+        $("#container").css({"background-color":"#ddd"});
+      }, 100);
+    } else if(this.word.length == 1 && this.word.charAt(0)==keypressed && this.wordsTyped == this.wordsInGame-1){
+      this.endTime = performance.now();
+      $('#score').html(this.name + ", sul kulus aega " + this.wordsInGame + " sõna kirjutamiseks " + (Math.round((this.endTime - this.startTime))/1000) + " sekundit.");
+      $('#wordDiv').html("Vajuta enter-klahvi, et uuesti alustada.");
+      this.wordsTyped++;
+      this.showInfo();
+      this.saveResult();
+      $(document).off('keypress');
+      $(document).on('keypress', (e)=>this.startNewGame(e.key));
     }
   }
 
-  startNewGame(){
-    let newGame = confirm("Alusta uut mängu?");
-    if(newGame == true){
+  showResults(){
+    $('#results').fadeToggle();
+    if($('#show-results').html() == "X"){
+      $('#show-results').html("TOP 15");
+    }else{
+      $('#show-results').html("X");
+    }
+    $('#results').html("");
+    $('#results').html("<h1>Edetabel<h1>");
+    for(let i = 0; i<=15; i++){
+      $('#results').append((i+1) +". "+this.results[i].name + " " + this.results[i].time + "<br>");
+    }
+  }
+
+  saveResult(){
+    let result = {
+      name: this.name,
+      time: (Math.round((this.endTime - this.startTime))/1000)
+    };
+
+    this.results.push(result);
+    this.results.sort((a, b) => parseFloat(a.time) - parseFloat(b.time));
+    localStorage.setItem("score", JSON.stringify(this.results));
+    console.log(this.results);
+  }
+
+  startNewGame(keypressed){
+    if(keypressed == "Enter"){
+      this.wordsTyped = 0;
+      $(document).off('keypress');
+      $('#score').html("");
       this.startTyper();
-    } else {
-      this.wordDiv.html("GAME OVER");
     }
   }
 }
 
-const typer = new Typer();
-prompt("Please enter your name", "Harry Potter");
-
-// kuvatakse trükitud sõnade arvu ja trükkimisele minevate sõnade arvu üleval vasakul nurgas "2 sõna 15-st trükitud."
-// valesti tähe trükkimisel muudab taust/sõna korraks värvi
-// moodustatakse edetabel kõige kiiremini sõnad trükkinud inimestest
-// Kasutaja saab ise valida kui palju sõnu trükkida tahab
+let typer = new Typer(name);
